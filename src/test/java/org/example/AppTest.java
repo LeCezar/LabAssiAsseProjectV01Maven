9bfe65f013e292b602d27778833f479b711ed3ef
LeCezar <lecezar2014@gmail.com>
2020-03-18 11:17:13 +0200

    Add the program and 2 test cases.

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,46 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
+import org.example.domain.Nota;
+import org.example.domain.Student;
+import org.example.domain.Tema;
+import org.example.repository.NotaXMLRepository;
+import org.example.repository.StudentXMLRepository;
+import org.example.repository.TemaXMLRepository;
+import org.example.service.Service;
+import org.example.validation.*;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
+
+    Validator<Student> studentValidator = new StudentValidator();
+    Validator<Tema> temaValidator = new TemaValidator();
+    Validator<Nota> notaValidator = new NotaValidator();
+
+    StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+    TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+    NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+    Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testIdForNull() {
+        String idStudent = null;
+        int result = service.saveStudent(idStudent,"Nume",935);
+        assertEquals("Null id test", 1, result);
+    }
+
+    @Test
+    public void testStudentNameForNull() {
+        String studentName = null;
+        int result = service.saveStudent("1",studentName,935);
+        assertEquals("Duplicate id test", 1, result);
     }
 }
